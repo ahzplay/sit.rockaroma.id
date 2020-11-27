47644cf1d581feef6353d75484d29d1c3353abdf
ahzplay <cute_killer@protonmail.com>
2020-11-27 10:14:05 +0700

    tambahan seeder videos

---
+++ database/seeders/VideoSeeder.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class VideoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('videos')->insert(
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma UDUT Eps.2 Pop Punk Bukan Bucin',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/-9cylpPqwZ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid3.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.2 | Summerlane',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/CDd_2e9oqVA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid2.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'is_active' => 1,
+                'title' => 'RockAroma Showcase #vol.1 | StereoWall',
+                'youtube_embeded' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_r5mcFoXY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                'thumb_path' => 'img/yt.vid1.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+        );
+    }
+}
