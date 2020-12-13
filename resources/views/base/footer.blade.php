08bc013db267b6ffaade86e0e02f873f53790f74
ahzplay <cute_killer@protonmail.com>
2020-12-13 18:39:36 +0700

    footer

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -1,11 +1,17 @@
 <footer class="footer" style="
     background-color: #FFD143;
     clear: both;
+    bottom: -100px;
     position: relative;
     height: 40px;
     margin-top:-40px;
 ">
     <div class="container" >
-        <span class="text-muted" >This is not live --S--I--T--.</span>
+        <div class="row">
+            <div class="col-md-6" style="color: #670404;font-size: 12px; font-weight: bold; margin-top: 1%">
+                ABOUT US &nbsp;&nbsp;&nbsp; TERM & CONDITION &nbsp;&nbsp;&nbsp; POLICY &nbsp;&nbsp;&nbsp; CONTACT US
+            </div>
+
+        </div>
     </div>
 </footer>
