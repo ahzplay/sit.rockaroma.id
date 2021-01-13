0c6d4f56cf414a98c5f34aa5dfe9fd526103b36d
ahzplay <cute_killer@protonmail.com>
2021-01-13 14:45:46 +0700

    tambahin share button dan footer

--- resources/views/articleDetailPage.blade.php
+++ resources/views/articleDetailPage.blade.php
@@ -124,8 +124,8 @@
                         <label style="color: white; font-weight: bold;">SHARE :</label>
                         <p>
                             {{--<i class="fa fa-instagram" style="font-size: 22px; color: #FFD143; padding-right: 7%;"></i>--}}
-                            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F60138212%2Fflutter-dynamic-suffix-text-for-textfield&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><i class="fa fa-facebook-square" style="font-size: 22px; color: #FFD143; padding-right: 7%;"></i></a>
-                            <i class="fa fa-twitter" style="font-size: 22px; color: #FFD143;"></i>
+                            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u={{url()->current()}}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><i class="fa fa-facebook-square" style="font-size: 22px; color: #FFD143; padding-right: 7%;"></i></a>
+                            <a target="_blank" href="https://twitter.com/intent/tweet?text={{url()->current()}}"><i class="fa fa-twitter" style="font-size: 22px; color: #FFD143;"></i></a>
                         </p>
                     </div>
                 </div>
--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -8,8 +8,8 @@
 ">
     <div class="container" >
         <div class="row">
-            <div class="col-md-6" style="color: #670404;font-size: 12px; font-weight: bold; margin-top: 1%">
-                ABOUT US &nbsp;&nbsp;&nbsp; TERM & CONDITION &nbsp;&nbsp;&nbsp; POLICY &nbsp;&nbsp;&nbsp; CONTACT US
+            <div class="col-md-6" style="color: #670404;font-size: 14px; font-weight: bold; margin-top: 1%; ">
+                <a href="{{url('landing-page')}}" style="text-decoration:none"><i class="fa fa-home"></i></a> &nbsp;&nbsp;&nbsp; <a href="{{url('article-page/1/topRow')}}" style="text-decoration:none">Article</a> &nbsp;&nbsp;&nbsp; <a href="{{url('video-page/1')}}" style="text-decoration:none">Video</a> &nbsp;&nbsp;&nbsp; <a href="{{url('register-profile-page')}}" style="text-decoration:none">Register</a> &nbsp;&nbsp;&nbsp; <a href="{{url('shop-page/1/1/?categoryId=0')}}" style="text-decoration:none">Shop</a>
             </div>
             <div class="col-md-5 text-right" style="color: #670404;font-size: 12px; margin-top: 0.8%">
 
