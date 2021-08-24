0f93a2d5574f5d6f77d21bb3a25097f19136ba41
ahzplay <cute_killer@protonmail.com>
2021-08-25 03:10:39 +0700

    bentrok menu.blade.php

--- resources/views/base/menu.blade.php
+++ resources/views/base/menu.blade.php
@@ -13,7 +13,7 @@
                             <a class="nav-link {{Session::get('menu-active-article')}}" href="{{url('article-page/1/topRow')}}">ARTICLE</a>
                             <a class="nav-link {{Session::get('menu-active-video')}}" href="{{url('video-page/1/1/?categoryId=0')}}">VIDEO</a>
                             <a class="nav-link {{Session::get('menu-active-register')}}" href="{{url('register-profile-page')}}">REGISTER</a>
-                            {{--<a class="nav-link {{Session::get('menu-active-shop')}}" href="{{url('shop-page/1/1/?categoryId=0')}}">SHOP</a>--}}
+                            <a class="nav-link {{Session::get('menu-active-shop')}}" href="{{url('shop-page/1/1/?categoryId=0')}}">SHOP</a>
                         </nav>
                     </div>
                     <div class="col-md-4 text-right">
